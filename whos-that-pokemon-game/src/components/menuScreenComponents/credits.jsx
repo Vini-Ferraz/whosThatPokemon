937a1ea94de786258830e2c7a46f8762277a4f0e
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-04-09 12:39:52 -0300

    test

--- whos-that-pokemon-game/src/components/menuScreenComponents/credits.jsx
+++ whos-that-pokemon-game/src/components/menuScreenComponents/credits.jsx
@@ -3,7 +3,7 @@
 export function Credits() {
   return (
     <p className="text-xl text-center">
-      Educational project non-profit. <br /> All rights reserved to Nintendo.
+      Educational project non-profit. <br /> All rights reserved to Nintendo
     </p>
   );
 }
